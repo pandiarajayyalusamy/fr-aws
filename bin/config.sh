7db57cf79c42d0087d77413cfbc4736ac9ea2b46
pandiarajayyalusamy <83716397+pandiarajayyalusamy@users.noreply.github.com>
2021-05-13 16:09:18 +0530

    Update config.sh

--- bin/config.sh
+++ bin/config.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Manage configurations for the ForgeRock platform. Copies configurations in git to the Docker/ folder
 #    Can optionally export configuration from running products and copy it back to the git /config folder.
 # This script is not supported by ForgeRock.
